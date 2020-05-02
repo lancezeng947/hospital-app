5a3e7ff03e9e0e776ef13d91349d53dcb007e36f
RXYKA <54512117+RXYKA@users.noreply.github.com>
2020-05-01 22:08:27 -0700

    Simple fix of pin

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import mysql.connector
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from joblib import dump, load 
+from joblib import dump, load
 import re
 
 
@@ -42,82 +42,94 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
-   return render_template("homepage.html")
+  return render_template("homepage.html")
 
 @app.route("/homepage.html")
 def home_banner():
-   return render_template("homepage.html")
+  return render_template("homepage.html")
 
 
 @app.route("/patient_page.html")
 def patient_page():
-   return render_template("patiencepage.html")
+  return render_template("patiencepage.html")
 
 @app.route("/doctor_page.html")
 def doctor_page():
-   return render_template("doctorpage.html")
+  return render_template("doctorpage.html")
 
 
 @app.route("/pin.html", methods = ['GET', 'POST'])
 def pin():
-	user_name = request.form['username']
-	phone_number = request.form['phone_num']
+  user_name = request.form['username']
+  phone_number = request.form['phone_num']
+  result = db.engine.execute("SELECT CONCAT(first_name, ' ', last_name) AS name, phone_number FROM patients WHERE CONCAT(first_name, ' ', last_name) = %s AND phone_number = %s", (user_name, phone_number))
+  result = result.fetchall()
+  print(result)
+  print(user_name)
+  print(phone_number)
+<<<<<<< HEAD
+  # print(result[0][0])
+  # print(result[0][0] == '')
 
+  try:
+     print(result[0][0])
+  except:
+    return render_template("patiencepage.html")
 
-	result = db.engine.execute("SELECT CONCAT(first_name, ' ', last_name) AS name, phone_number FROM patients \
-		WHERE CONCAT(first_name, ' ', last_name) = %s AND phone_number = %s", (user_name, phone_number))
+  if result[0][0] == ' ':
+    return render_template("patiencepage.html")
+  else:
+    return render_template("pin0.html")
+=======
+  print(result[0][0])
+  print(result[0][0] == '')
+  if result[0][0] == ' ':
+    return render_template("patiencepage.html")
+  else:
+    return render_template("pin.html")
+>>>>>>> c0e4c19067c456e8304a4125ce44f315d53f09d3
 
-	result = result.fetchall()
-	print(result)
-
-	print(user_name)
-	print(phone_number)
-
-	print(result[0][0])
-	print(result[0][0] == ' ')
-
-	if result[0][0] == ' ':
-		return render_template("patiencepage.html")
-	else:
-		return render_template("pin.html")
 
 @app.route("/pprofile.html")
 def pprofile():
-   return render_template("pprofile.html")
+  return render_template("pprofile.html")
 
 @app.route("/precord.html")
 def precord():
-   return render_template("precord.html")
+  return render_template("precord.html")
 
 @app.route("/pdoc.html")
 def pdoc():
-   return render_template("pdoc.html")
+  return render_template("pdoc.html")
 
 @app.route("/pinsur.html")
 def pinsur():
-   return render_template("pinsur.html")
+  return render_template("pinsur.html")
 
 @app.route("/pmed.html")
 def pmed():
-   return render_template("pmed.html")
+  return render_template("pmed.html")
 
 @app.route("/phos.html")
 def phos():
-   return render_template("phos.html")
+  return render_template("phos.html")
 
 @app.route("/nearbyhos.html")
 def nearbyhos():
-   return render_template("nearbyhos.html")
+  return render_template("nearbyhos.html")
 
 @app.route("/docrecommend.html")
 def docrecommend():
-   return render_template("docrecommend.html")
+  return render_template("docrecommend.html")
 
 @app.route("/support.html")
 def support():
-   return render_template("support.html")
+  return render_template("support.html")
 
+@app.route("/pin0.html")
+def pin0():
+  return render_template("pin0.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+  app.run(host="0.0.0.0", debug=True)
