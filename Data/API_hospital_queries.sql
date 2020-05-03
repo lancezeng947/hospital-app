18dc4838a4df16021d7ae247486c55bc02c1e30d
Lance Zeng <lancezeng@berkeley.edu>
2020-05-03 15:19:26 -0700

    table upload

--- Data/API_hospital_queries.sql
+++ Data/API_hospital_queries.sql
@@ -11,15 +11,23 @@ LEFT JOIN hospitals h ON ih.hospital_id=h.id
 WHERE p.id=4
     AND SUBSTRING(p.zip,1,2)=SUBSTRING(h.zip,1,2);
 
+SELECT i.id AS insurance_ID, i.name
+                FROM patients p
+                LEFT JOIN insurance i ON p.insurance_id = i.id
+                WHERE p.id=1;
+
+
+
 #Hospitals Near Me
-SELECT DISTINCT p.first_name, i.name AS insurance_name,p.zip,
+SELECT i.name AS insurance_name,
                 h.name AS hospital_name, h.street,h.city,h.state, h.zip, h.type,h.website, h.phone_number
 FROM patients p
 LEFT JOIN insurance i ON p.insurance_id = i.id
 LEFT JOIN insurance_hospital ih ON i.id = ih.insurance_id
 LEFT JOIN hospitals h ON ih.hospital_id=h.id
-WHERE p.id=4
-    AND SUBSTRING(p.zip,1,2)=SUBSTRING(h.zip,1,2);
+WHERE p.id=3 AND h.state = p.state;
+
+SELECT * FROM insurance_hospital WHERE insurance_id = 22;
 
 #Doctors Near Me
 SELECT DISTINCT p.first_name, i.name AS insurance_name,p.zip,
@@ -34,9 +42,9 @@ WHERE p.id=4
     AND SUBSTRING(p.zip,1,2)=SUBSTRING(h.zip,1,2);
 
 #My Community
-SELECT DISTINCT sub.name AS disease_name, sub.first_name,sub.last_name,sub.email,sub.zip
+SELECT sub.name AS disease_name, sub.first_name,sub.last_name,sub.email,sub.zip
 FROM (
-    SELECT DISTINCT p.first_name, p.last_name,p.email, p.zip,v.disease_id, d.name,SUBSTRING(zip,1,1) AS sub_zip
+    SELECT p.first_name, p.last_name,p.email, p.zip,v.disease_id, d.name,SUBSTRING(zip,1,1) AS sub_zip
     FROM patients p
     JOIN visits v on p.id = v.patient_id
     JOIN diseases d on v.disease_id = d.id
@@ -88,7 +96,7 @@ SELECT DISTINCT v.date,
        p2.first_name AS doctor_firstname,
        p2.last_name AS doctor_lastname,
        t.quantity,
-       t.drug_id
+       t.drug_id, d2.name, d.name
 FROM patients p
 JOIN visits v on p.id = v.patient_id
 JOIN hospitals h on v.hospital_id = h.id
@@ -100,11 +108,37 @@ JOIN drugs d2 on d2.id=t.drug_id
 WHERE p.id=1 AND drug_id<>0
 ORDER BY 1 DESC;
 
+SELECT v.date,
+         h.name AS hospital_name,
+         CONCAT(p2.first_name, ' ', p2.last_name) AS doctor_name,
+         d2.name,
+         t.quantity,
+         t.drug_id
+  FROM patients p
+  JOIN visits v on p.id = v.patient_id
+  JOIN hospitals h on v.hospital_id = h.id
+  JOIN physicians p2 on v.doctor_id = p2.id
+  JOIN visit_treatment vt on v.id = vt.visit_id
+  JOIN treatment t on vt.treatment_id = t.id
+  JOIN diseases d on v.disease_id = d.id
+  JOIN drugs d2 on d2.id=t.drug_id
+  WHERE p.id=1 AND drug_id<>0
+  ORDER BY 1 DESC;
+
+SELECT DISTINCT CONCAT(p.first_name, ' ', p.last_name), h.name,
+                CONCAT(h.street, ' ', h.city, ', ', h.state, ' ', h.zip), specialty FROM patients JOIN visits v on patients.id = v.patient_id
+ JOIN hospitals h on v.hospital_id = h.id
+ JOIN physicians p on v.doctor_id = p.id
+ WHERE patients.id = 1;
 
 
+SELECT DISTINCT h.name,
+                CONCAT(h.street, ' ', h.city, ', ', h.state, ' ', h.zip), h.phone_number, h.website FROM patients JOIN visits v on patients.id = v.patient_id
+ JOIN hospitals h on v.hospital_id = h.id
+ JOIN physicians p on v.doctor_id = p.id
+ WHERE patients.id = 1;
 
-
-
+SELECT * FROM patients LIMIT 2
 
 
 
--- index.py
+++ index.py
@@ -125,27 +125,104 @@ def pprofile():
 
 @app.route("/precord.html")
 def precord():
-  return render_template("precord.html")
+  user_id = session.get('user_id')
+  result = db.engine.execute("SELECT DISTINCT v.date, \
+         h.name AS hospital_name, \
+         CONCAT(p2.first_name,' ', p2.last_name) AS doctor_name, \
+         CONCAT(n.first_name,' ', n.last_name) AS nurse_name, \
+         t.activity, d2.name AS drug_name,  d.name \
+        FROM patients p \
+          JOIN visits v on p.id = v.patient_id \
+          JOIN hospitals h on v.hospital_id = h.id \
+          JOIN physicians p2 on v.doctor_id = p2.id \
+          JOIN nurses n on v.nurse_id = n.id \
+          JOIN visit_treatment vt on v.id = vt.visit_id \
+          JOIN treatment t on vt.treatment_id = t.id \
+          JOIN diseases d on v.disease_id = d.id \
+          JOIN drugs d2 on d2.id=t.drug_id \
+          WHERE p.id=%s \
+          ORDER BY 1 DESC", user_id);
+
+  return render_template("precord.html", records = result)
 
 @app.route("/pdoc.html")
 def pdoc():
-  return render_template("pdoc.html")
+
+  user_id = session.get('user_id')
+  result = db.engine.execute("SELECT DISTINCT CONCAT(p.first_name, ' ', p.last_name), h.name, \
+                CONCAT(h.street, ' ', h.city, ', ', h.state, ' ', h.zip), specialty FROM patients JOIN visits v on patients.id = v.patient_id \
+                JOIN hospitals h on v.hospital_id = h.id \
+               JOIN physicians p on v.doctor_id = p.id \
+               WHERE patients.id = %s", user_id)
+
+  return render_template("pdoc.html", records = result)
 
 @app.route("/pinsur.html")
 def pinsur():
-  return render_template("pinsur.html")
+
+  user_id = session.get('user_id')
+  print(user_id)
+
+  result = db.engine.execute("SELECT i.id AS insurance_ID, i.name \
+                FROM patients p \
+                LEFT JOIN insurance i ON p.insurance_id = i.id \
+                WHERE p.id=%s", user_id)
+
+  result = result.fetchall()
+  print(result)
+
+
+  return render_template("pinsur.html", records = result)
 
 @app.route("/pmed.html")
 def pmed():
-  return render_template("pmed.html")
+  
+  user_id = session.get('user_id')
+  result = db.engine.execute("SELECT v.date, \
+         h.name AS hospital_name, \
+         CONCAT(p2.first_name, ' ', p2.last_name) AS doctor_name, \
+         d2.name, \
+         t.quantity, \
+         t.drug_id \
+  FROM patients p \
+  JOIN visits v on p.id = v.patient_id \
+  JOIN hospitals h on v.hospital_id = h.id \
+  JOIN physicians p2 on v.doctor_id = p2.id \
+  JOIN visit_treatment vt on v.id = vt.visit_id \
+  JOIN treatment t on vt.treatment_id = t.id \
+  JOIN diseases d on v.disease_id = d.id \
+  JOIN drugs d2 on d2.id=t.drug_id \
+  WHERE p.id=%s AND drug_id<>0 \
+  ORDER BY 1 DESC", user_id)
+
+
+  return render_template("pmed.html", records = result)
 
 @app.route("/phos.html")
 def phos():
-  return render_template("phos.html")
+  user_id = session.get('user_id')
+  result = db.engine.execute("SELECT DISTINCT h.name, \
+                CONCAT(h.street, ' ', h.city, ', ', h.state, ' ', h.zip), h.phone_number, h.website FROM patients JOIN visits v on patients.id = v.patient_id \
+                   JOIN hospitals h on v.hospital_id = h.id \
+                   JOIN physicians p on v.doctor_id = p.id \
+                   WHERE patients.id = %s", user_id)
+  return render_template("phos.html", records = result)
 
 @app.route("/nearbyhos.html")
 def nearbyhos():
-  return render_template("nearbyhos.html")
+
+  user_id = session.get('user_id')  
+
+  result = db.engine.execute("SELECT \
+                h.name AS hospital_name, CONCAT(h.street, ' ', h.city, ', ', h.state, ' ', h.zip) AS address, h.phone_number, h.website \
+                FROM patients p \
+                LEFT JOIN insurance i ON p.insurance_id = i.id \
+                LEFT JOIN insurance_hospital ih ON i.id = ih.insurance_id \
+                LEFT JOIN hospitals h ON ih.hospital_id=h.id \
+                WHERE p.id=%s AND h.state = p.state", user_id)
+
+
+  return render_template("nearbyhos.html", records = result)
 
 @app.route("/docrecommend.html")
 def docrecommend():
@@ -153,7 +230,30 @@ def docrecommend():
 
 @app.route("/support.html")
 def support():
-  return render_template("support.html")
+
+  user_id = session.get('user_id')  
+
+  result = db.engine.execute("SELECT CONCAT(sub.first_name,' ', sub.last_name) AS name, sub.phone_number, sub.email, sub.name AS disease_name, sub.zip \
+    FROM ( \
+      SELECT p.first_name, p.last_name,p.phone_number, p.email, p.zip,v.disease_id, d.name,SUBSTRING(zip,1,1) AS sub_zip \
+      FROM patients p \
+      JOIN visits v on p.id = v.patient_id \
+      JOIN diseases d on v.disease_id = d.id \
+      WHERE v.disease_id IN ( \
+          SELECT disease_id \
+          FROM patients \
+          JOIN visits v2 on patients.id = v2.patient_id \
+          WHERE patients.id=%s AND disease_id<>0) \
+      AND p.id<>%s) AS sub \
+    JOIN \
+      (SELECT SUBSTRING(zip,1,1) AS sub_zip \
+      FROM patients p2 \
+      WHERE p2.id=%s ) sub1 \
+      ON sub.sub_zip=sub1.sub_zip", (user_id, user_id, user_id))
+
+  print(result)
+
+  return render_template("support.html", records = result)
 
 @app.route("/privacy.html")
 def privacy():
