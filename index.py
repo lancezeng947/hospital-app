8c03fe48a1725f44a9cc49571826ae7f976ceb82
lancezeng947 <43800441+lancezeng947@users.noreply.github.com>
2020-05-02 19:29:35 -0700

    initial flask page

--- index.py
+++ index.py
@@ -8,6 +8,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from joblib import dump, load
 import re
+from flask import g, session
 
 
 # specify database configurations
@@ -37,11 +38,14 @@ print(connection_str)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = connection_str
+app.config["SECRET_KEY"] = "testsecretkey"
 db = SQLAlchemy(app)
 
 
 @app.route("/")
 def home():
+  session['key'] = 'value'
+
   return render_template("homepage.html")
 
 @app.route("/homepage.html")
@@ -60,36 +64,64 @@ def doctor_page():
 
 @app.route("/pin.html", methods = ['GET', 'POST'])
 def pin():
+
   user_name = request.form['username']
   phone_number = request.form['phone_num']
-  result = db.engine.execute("SELECT CONCAT(first_name, ' ', last_name) AS name, phone_number FROM patients WHERE CONCAT(first_name, ' ', last_name) = %s AND phone_number = %s", (user_name, phone_number))
+  result = db.engine.execute("SELECT id, CONCAT(first_name, ' ', last_name) AS name, phone_number FROM patients WHERE CONCAT(first_name, ' ', last_name) = %s AND phone_number = %s", (user_name, phone_number))
   result = result.fetchall()
-  print(result)
-  print(user_name)
-  print(phone_number)
+
+
+  #print(result)
+  #print(user_name)
+  #print(phone_number)
   # print(result[0][0])
   # print(result[0][0] == '')
 
+  print(result)
   try:
-     print(result[0][0])
+    session['user_id'] = result[0][0]
+    print(result[0][0])
+
+    if result[0][0] == ' ':
+      return render_template("patiencepage.html")
+    else:
+      print('hi')
+      return render_template('pin0.html')
+
   except:
+    #Login failed
     return render_template("patiencepage.html")
 
-  if result[0][0] == ' ':
-    return render_template("patiencepage.html")
-  else:
-    return render_template("pin0.html")
+  '''
   print(result[0][0])
   print(result[0][0] == '')
   if result[0][0] == ' ':
     return render_template("patiencepage.html")
   else:
     return render_template("pin.html")
+  '''
 
+
+@app.route("/pin0.html")
+def pin0():
+  return render_template("pin0.html") 
 
 @app.route("/pprofile.html")
 def pprofile():
-  return render_template("pprofile.html")
+  user_id = session.get('user_id')
+  print(user_id)
+
+  result = db.engine.execute("SELECT CONCAT(first_name, ' ', last_name) AS name, phone_number, CONCAT(street, ' ', city, ', ', state) AS address, zip \
+    FROM patients WHERE id = %s", user_id)
+  result = result.fetchall()
+
+  print(result)
+  name = result[0][0]
+  phoneNum = result[0][1]
+  address = result[0][2]
+  zipcode = result[0][3]
+
+  return render_template("pprofile.html", name = name, phoneNum = phoneNum, address = address, zipcode = zipcode)
 
 @app.route("/precord.html")
 def precord():
@@ -123,9 +155,7 @@ def docrecommend():
 def support():
   return render_template("support.html")
 
-@app.route("/pin0.html")
-def pin0():
-  return render_template("pin0.html")
+
 
 
 if __name__ == "__main__":
